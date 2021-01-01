db79ef8575b150cac5aa3a002c917bd212065f3f
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-01-01 20:54:31 +0900

    Mod: 홈화면 개선 및 배경색 변경 중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,25 +12,31 @@ export default class Home extends Component {
   render() {
     return (
       <>
-        <Container fluid="sm" className="ct">
-          <div className="Main-intro">
-            <img src={logoinv} className="logo pb-3 img-view-only no-drag" alt="Aztra" />
-            <h1 className="no-drag text-white">
-              미래를 바꿀 디스코드 관리봇, Aztra
-            </h1>
-            <h2 className="no-drag text-white pb-5">
-              다채롭고 깔끔한 디스코드 서버를 만들 수 있도록 도와드리겠습니다.
-            </h2>
-            <Button variant="aztra" size="lg" className="Main-button shadow" disabled={process.env.NODE_ENV === "production"}>
-              {
-                process.env.NODE_ENV === "production"
-                  ? "곧 공개됩니다"
-                  : "초대하기"
-              }
-            </Button>
-            <Button as={ScrollLink} to="main-features" spy={true} smooth={true} offset={50} duration={500} variant="light" size="lg" className="Main-button shadow">
-              자세히 알아보기
-            </Button>
+        <Container fluid="sm">
+          <div className="Main-intro d-flex align-items-center justify-content-center">
+            <div>
+              <h1 className="no-drag text-white" style={{
+                fontSize: '25pt',
+                wordBreak: 'keep-all'
+              }}>
+                미래를 바꿀 디스코드 관리봇, Aztra
+              </h1>
+              <h2 className="no-drag text-white pt-2 pb-5 font-weight-lighter" style={{
+                fontSize: '14pt'
+              }}>
+                다채롭고 깔끔한 디스코드 서버를 만들 수 있도록 도와드리겠습니다.
+              </h2>
+              <Button variant="aztra" size="lg" className="Main-button shadow-lg" disabled={process.env.NODE_ENV === "production"}>
+                {
+                  process.env.NODE_ENV === "production"
+                    ? "곧 공개됩니다"
+                    : "초대하기"
+                }
+              </Button>
+              <Button as={ScrollLink} to="main-features" spy={true} smooth={true} offset={50} duration={500} variant="light" size="lg" className="Main-button shadow-lg">
+                자세히 알아보기
+              </Button>
+            </div>
           </div>
         </Container>
         <Container fluid id="main-features" className="bg-dark text-white Main-features" style={{
@@ -58,7 +64,7 @@ export default class Home extends Component {
                 <p>Aztra 대시보드는 모바일에서도 언제 어디서나 사용하기 편하도록 모바일 환경에도 신경써서 개발하고 있습니다.</p>
               </Col>
             </Row>
-            <Row className="align-items-center px-3" style={{ marginBottom: 200 }}>
+            <Row className="align-items-center px-3" style={{ marginBottom: 80 }}>
               <Col md={6}>
                 <img alt="멤버 관리" src={FeatImg3} className="shadow mb-4" />
               </Col>
