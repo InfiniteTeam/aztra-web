c31f5494e3c26abf726e67ff10c806d3d81be222
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-01-04 19:09:43 +0900

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,9 @@
 const { override, addWebpackModuleRule } = require("customize-cra")
 
 module.exports = override(
-    addWebpackModuleRule({test: /\.(md|txt)$/, use: 'raw-loader'})
+    addWebpackModuleRule({test: /\.(md|txt)$/, use: 'raw-loader'}),
+    config => ({
+        ...config,
+        devtool: 'source-map'
+    })
 )
